c1775c70688cb73ecfb7f8e2b1cff0eee1fb4fa4
Shankar <shankarb3061997@gmail.com>
2025-10-28 06:25:10 +0530

    navigate to restaurant page

--- lib/components/restaurant_section.dart
+++ lib/components/restaurant_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:yummy/constants.dart';
 import '../components/components.dart';
 import '../models/models.dart';
 
@@ -7,12 +9,11 @@ class RestaurantSection extends StatelessWidget {
   final CartManager cartManager;
   final OrderManager orderManager;
 
-  const RestaurantSection({
-    super.key, 
-    required this.restaurants,
-    required this.cartManager,
-    required this.orderManager
-  });
+  const RestaurantSection(
+      {super.key,
+      required this.restaurants,
+      required this.cartManager,
+      required this.orderManager});
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +26,7 @@ class RestaurantSection extends StatelessWidget {
             padding: EdgeInsets.only(left: 16.0, bottom: 8.0),
             child: Text(
               'Food near me',
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
           ),
           SizedBox(
@@ -37,14 +36,15 @@ class RestaurantSection extends StatelessWidget {
               itemCount: restaurants.length,
               itemBuilder: (context, index) {
                 return SizedBox(
-                  width: 300,
-                  child: RestaurantLandscapeCard(
-                    restaurant: restaurants[index],
-                    onTap: () {
-                      // TODO: Navigate to Restaurant
-                    },
-                  )
-                );
+                    width: 300,
+                    child: RestaurantLandscapeCard(
+                      restaurant: restaurants[index],
+                      onTap: () {
+                        context.go(
+                          "/${YummyTab.home.value}/restaurant/${restaurants[index].id}",
+                        );
+                      },
+                    ));
               },
             ),
           ),
